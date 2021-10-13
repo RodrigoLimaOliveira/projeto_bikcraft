ad5c845fefc5c368a8e9d5d3e01038a95c7ec577
Rodrigo Lima <rodrigo.lima.oliveira@outlook.com>
2021-10-13 14:11:21 -0300

    alterada cor no css do form de contato e produtos

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ if (window.SimpleForm) {
     new SimpleForm ({
         form: ".formphp",
         button: "#enviar",
-        erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o e-mail contato@bikcraft.com</p></div>",
-        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>",
+        erro: "<div id='form-erro'><h3>Erro no envio!</h3><p>Um erro ocorreu, tente para o e-mail contato@bikcraft.com</p></div>",
+        sucesso: "<div id='form-sucesso'><h3>Formulário enviado com sucesso</h3><p>Em breve eu entro em contato com você.</p></div>",
     });
 }
 
